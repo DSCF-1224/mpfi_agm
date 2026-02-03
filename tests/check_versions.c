8580a86b2afac35722973c07aa6c5b7bbce919b5
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-04 06:36:51 +0900

    test: add C standard version info to version checker
    Display __STDC_VERSION__ and __STDC_ISO_10646__ macros
    to verify the C standard version used during compilation
    alongside library versions.

--- tests/check_versions.c
+++ tests/check_versions.c
@@ -6,6 +6,12 @@
 
 int main(void)
 {
+#ifdef __STDC_VERSION__
+    printf( "__STDC_VERSION__   : %ld\n"   , __STDC_VERSION__ );
+#endif
+#ifdef __STDC_ISO_10646__
+    printf( "__STDC_ISO_10646__ : %ld\n\n" , __STDC_ISO_10646__ );
+#endif
     printf( "GMP  VERSION; %s\n", gmp_version        );
     printf( "MPFR VERSION; %s\n", mpfr_get_version() );
     printf( "MPFI VERSION; %s\n", mpfi_get_version() );
