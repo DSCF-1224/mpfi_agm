982f23032cf3834726489bfae568f7e2e6e2a9c9
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-05 06:40:32 +0900

    test: add AGM step function test skeleton
    Add initial test file for mpfi_agm_step function.
    Currently prints a test marker; ready for test implementation.

---
+++ tests/check_step.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mpfr.h>
+#include <mpfi.h>
+#include "../src/mpfi_agm.h"
+
+int main(void)
+{
+    printf("%s\n", "TEST");
+    return EXIT_SUCCESS;
+}
