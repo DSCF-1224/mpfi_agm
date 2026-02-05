f4bfea899310ff75e6c56ea56b97cd92623d54b8
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-06 07:17:12 +0900

    test: implement AGM step test case
    Add validation for mpfi_agm_step with equal inputs (1,1).
    Use assertions to verify correctness.

--- tests/check_step.c
+++ tests/check_step.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +8,30 @@
 
 int main(void)
 {
-    printf("%s\n", "TEST");
+    const mpfr_prec_t mpfi_prec = 53;
+
+
+
+    mpfi_t x, y, res_a, res_g;
+
+
+
+    mpfi_init2(x, mpfi_prec);
+    mpfi_init2(y, mpfi_prec);
+
+    mpfi_init2(res_a, mpfi_prec);
+    mpfi_init2(res_g, mpfi_prec);
+
+    mpfi_init_set_ui(x, 1UL);
+    mpfi_init_set_ui(y, 1UL);
+
+    mpfi_agm_step(res_a, res_g, x, y);
+
+    assert( mpfi_cmp_ui(res_a, 1UL) == 0 );
+    assert( mpfi_cmp_ui(res_g, 1UL) == 0 );
+
+    mpfi_clear(x);
+    mpfi_clear(y);
+
     return EXIT_SUCCESS;
 }
