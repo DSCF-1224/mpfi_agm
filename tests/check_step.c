8020b64eadb1e5b91c12d1b68f72e4b4d8ff92a6
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-08 06:59:24 +0900

    refactor: add _case suffix to test primitive
    Rename to test_same_input_case_ui
    to clarify it executes a single test case,
    distinguishing it from the multi-case wrapper.

--- tests/check_step.c
+++ tests/check_step.c
@@ -8,7 +8,7 @@
 
 
 
-void test_same_input_ui(const mpfr_prec_t mpfi_prec, const unsigned long input)
+void test_same_input_case_ui(const mpfr_prec_t mpfi_prec, const unsigned long input)
 {
     mpfi_t x, res_a, res_g;
 
@@ -37,7 +37,7 @@ void test_same_input(const mpfr_prec_t mpfi_prec)
 {
     for (unsigned long i = 0; i < 6; i++)
     {
-        test_same_input_ui(mpfi_prec, i);
+        test_same_input_case_ui(mpfi_prec, i);
     }
 }
 
