7b17c7360f7cca2b9ebe3c512157e9b53d844c1b
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-07 09:15:36 +0900

    refactor: parameterize AGM step test
    Extract test logic into reusable test_unit function
    with configurable precision and input values.

--- tests/check_step.c
+++ tests/check_step.c
@@ -6,32 +6,31 @@
 #include <mpfi.h>
 #include "../src/mpfi_agm.h"
 
-int main(void)
+
+void test_unit(const mpfr_prec_t mpfi_prec, const unsigned long input)
 {
-    const mpfr_prec_t mpfi_prec = 53;
-
-
-
-    mpfi_t x, y, res_a, res_g;
+    mpfi_t x, res_a, res_g;
 
 
 
     mpfi_init2(x, mpfi_prec);
-    mpfi_init2(y, mpfi_prec);
 
     mpfi_init2(res_a, mpfi_prec);
     mpfi_init2(res_g, mpfi_prec);
 
-    mpfi_init_set_ui(x, 1UL);
-    mpfi_init_set_ui(y, 1UL);
+    mpfi_init_set_ui(x, input);
 
-    mpfi_agm_step(res_a, res_g, x, y);
+    mpfi_agm_step(res_a, res_g, x, x);
 
-    assert( mpfi_cmp_ui(res_a, 1UL) == 0 );
-    assert( mpfi_cmp_ui(res_g, 1UL) == 0 );
+    assert( mpfi_cmp_ui(res_a, input) == 0 );
+    assert( mpfi_cmp_ui(res_g, input) == 0 );
 
     mpfi_clear(x);
-    mpfi_clear(y);
+}
+
+int main(void)
+{
+    test_unit(53, 1UL);
 
     return EXIT_SUCCESS;
 }
