261b4388300c3857c92ee5ab321604a61d0e2f2c
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-11 12:52:05 +0900

    feat: add workspace variant mpfi_agm_step_tmp
    Add mpfi_agm_step_tmp function that accepts pre-allocated temporary variables, enabling efficient memory reuse across iterations.
    Refactor mpfi_agm_step to call mpfi_agm_step_tmp internally.
    
    - mpfi_agm_step_tmp: caller provides tmp_a, tmp_g workspace
    - mpfi_agm_step: convenience wrapper (allocates internally)
    - Reduces code duplication between variants
    - Enables performance optimization for iterative algorithms

--- src/mpfi_agm.h
+++ src/mpfi_agm.h
@@ -3,6 +3,17 @@
 
 #include <mpfi.h>
 
+
+
+static void mpfi_agm_step_tmp(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_ptr tmp_a, mpfi_ptr tmp_g, mpfi_srcptr op1, mpfi_srcptr op2)
+{
+    mpfi_add(tmp_a, op1, op2); mpfi_div_ui(res_a, tmp_a, 2UL);
+
+    mpfi_mul(tmp_g, op1, op2); mpfi_sqrt(res_g, tmp_g);
+}
+
+
+
 static void mpfi_agm_step(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_srcptr op1, mpfi_srcptr op2)
 {
     mpfi_t tmp_a, tmp_g;
@@ -10,9 +21,7 @@ static void mpfi_agm_step(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_srcptr op1, mpfi_
     mpfi_init2(tmp_a, mpfi_get_prec(res_a));
     mpfi_init2(tmp_g, mpfi_get_prec(res_g));
 
-    mpfi_add(tmp_a, op1, op2); mpfi_div_ui(res_a, tmp_a, 2UL);
-
-    mpfi_mul(tmp_g, op1, op2); mpfi_sqrt(res_g, tmp_g);
+    mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, op1, op2);
 
     mpfi_clear(tmp_a);
     mpfi_clear(tmp_g);
