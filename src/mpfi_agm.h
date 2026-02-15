1098d3d11e58a634d63f441fbc1ff773997f1577
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-15 10:44:28 +0900

    feat: add mpfi_agm function for complete AGM computation
    Implement full AGM algorithm that iterates until convergence,
    returning the AGM value as an interval.
    
    - If inputs intersect: return their union immediately
    - Otherwise: iterate AGM steps until a and g intersect
    - Stop when arithmetic and geometric means converge
    - Maximum iterations: precision bits
    - Return union of converged intervals as result
    - Uses mpfi_agm_step_tmp internally for efficiency

--- src/mpfi_agm.h
+++ src/mpfi_agm.h
@@ -14,6 +14,50 @@ static void mpfi_agm_step_tmp(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_ptr tmp_a, mp
 
 
 
+static void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2)
+{
+    const mpfr_prec_t mpfi_prec = mpfi_get_prec(res);
+
+    mpfi_intersect(res, op1, op2);
+
+    if ( !mpfi_is_empty(res) )
+    {
+        mpfi_union(res, op1, op2); return;
+    }
+
+    mpfi_t intersection_ag, res_a, res_g, tmp_a, tmp_g, union_ag;
+
+    mpfi_init_set(intersection_ag, res);
+
+    mpfi_init2( res_a    , mpfi_prec ); mpfi_set(res_a, op1);
+    mpfi_init2( res_g    , mpfi_prec ); mpfi_set(res_g, op2);
+    mpfi_init2( tmp_a    , mpfi_prec );
+    mpfi_init2( tmp_g    , mpfi_prec );
+    mpfi_init2( union_ag , mpfi_prec );
+
+    for (mpfr_prec_t i = 0; i < mpfi_prec; i++)
+    {
+        mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, res_a, res_g);
+
+        mpfi_intersect(intersection_ag, res_a, res_g);
+
+        if ( mpfi_is_empty(intersection_ag) ) continue;
+
+        break;
+    }
+
+    mpfi_union(res, res_a, res_g);
+
+    mpfi_clear( intersection_ag );
+    mpfi_clear( res_a           );
+    mpfi_clear( res_g           );
+    mpfi_clear( tmp_a           );
+    mpfi_clear( tmp_g           );
+    mpfi_clear( union_ag        );
+}
+
+
+
 static void mpfi_agm_step(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_srcptr op1, mpfi_srcptr op2)
 {
     mpfi_t tmp_a, tmp_g;
