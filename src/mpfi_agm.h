0311521c42f5e97128f721b985048e0301051f0e
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-18 23:40:13 +0900

    fix: prevent aliasing bug in mpfi_agm iteration
    Fix incorrect in-place iteration by using separate _prev variables to hold previous iteration values.
    Prevents undefined behavior when reading and writing the same intervals.
    
    - Add res_a_prev and res_g_prev to store previous iteration
    - Pass _prev to mpfi_agm_step_tmp as inputs (avoid aliasing)
    - Update _prev values after each iteration
    - Properly clear all allocated variables

--- src/mpfi_agm.h
+++ src/mpfi_agm.h
@@ -25,23 +25,29 @@ static void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2)
         mpfi_union(res, op1, op2); return;
     }
 
-    mpfi_t intersection_ag, res_a, res_g, tmp_a, tmp_g, union_ag;
+    mpfi_t intersection_ag, res_a, res_a_prev, res_g, res_g_prev, tmp_a, tmp_g, union_ag;
 
     mpfi_init_set(intersection_ag, res);
 
-    mpfi_init2( res_a    , mpfi_prec ); mpfi_set(res_a, op1);
-    mpfi_init2( res_g    , mpfi_prec ); mpfi_set(res_g, op2);
-    mpfi_init2( tmp_a    , mpfi_prec );
-    mpfi_init2( tmp_g    , mpfi_prec );
-    mpfi_init2( union_ag , mpfi_prec );
+    mpfi_init2( res_a      , mpfi_prec );
+    mpfi_init2( res_a_prev , mpfi_prec ); mpfi_set(res_a_prev, op1);
+    mpfi_init2( res_g      , mpfi_prec );
+    mpfi_init2( res_g_prev , mpfi_prec ); mpfi_set(res_g_prev, op2);
+    mpfi_init2( tmp_a      , mpfi_prec );
+    mpfi_init2( tmp_g      , mpfi_prec );
+    mpfi_init2( union_ag   , mpfi_prec );
 
     for (mpfr_prec_t i = 0; i < mpfi_prec; i++)
     {
-        mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, res_a, res_g);
+        mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, res_a_prev, res_g_prev);
 
         mpfi_intersect(intersection_ag, res_a, res_g);
 
-        if ( mpfi_is_empty(intersection_ag) ) continue;
+        if ( mpfi_is_empty(intersection_ag) )
+        {
+            mpfi_set(res_a_prev, res_a);
+            mpfi_set(res_g_prev, res_g);
+        }
 
         break;
     }
@@ -50,7 +56,9 @@ static void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2)
 
     mpfi_clear( intersection_ag );
     mpfi_clear( res_a           );
+    mpfi_clear( res_a_prev      );
     mpfi_clear( res_g           );
+    mpfi_clear( res_g_prev      );
     mpfi_clear( tmp_a           );
     mpfi_clear( tmp_g           );
     mpfi_clear( union_ag        );
