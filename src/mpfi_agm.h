dce6afcd280728049c35e91580aeec15c3051f24
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-22 12:07:12 +0900

    refactor: convert to compiled library and cleanup
    Convert mpfi_agm from header-only to compiled library with proper separation of interface and implementation.
    
    Library Structure:
    - Create src/mpfi_agm.c with all function implementations
    - Convert mpfi_agm.h to declarations only (public API)
    - Remove static keywords (functions now have external linkage)
    - Remove unused union_ag variable
    
    Build System:
    - Change from INTERFACE to compiled library target
    - Update target properties: INTERFACE → PRIVATE
    - Single compilation of implementations (faster builds)
    - Smaller binary size (no code duplication)
    
    Benefits:
    - Faster compilation (implementations compiled once)
    - Standard library structure (header + source)
    - Better encapsulation and maintainability
    - Cleaner public API in header file
    
    git merge --squash develop_refactor
    Squashes 3 commits: aca01d f09cd7b 36b656b

--- src/mpfi_agm.h
+++ src/mpfi_agm.h
@@ -5,80 +5,10 @@
 
 
 
-static void mpfi_agm_step_tmp(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_ptr tmp_a, mpfi_ptr tmp_g, mpfi_srcptr op1, mpfi_srcptr op2)
-{
-    mpfi_add(tmp_a, op1, op2); mpfi_div_ui(res_a, tmp_a, 2UL);
+void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2);
 
-    mpfi_mul(tmp_g, op1, op2); mpfi_sqrt(res_g, tmp_g);
-}
+void mpfi_agm_step(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_srcptr op1, mpfi_srcptr op2);
 
-
-
-static void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2)
-{
-    const mpfr_prec_t mpfi_prec = mpfi_get_prec(res);
-
-    mpfi_intersect(res, op1, op2);
-
-    if ( !mpfi_is_empty(res) )
-    {
-        mpfi_union(res, op1, op2); return;
-    }
-
-    mpfi_t intersection_ag, res_a, res_a_prev, res_g, res_g_prev, tmp_a, tmp_g, union_ag;
-
-    mpfi_init_set(intersection_ag, res);
-
-    mpfi_init2( res_a      , mpfi_prec );
-    mpfi_init2( res_a_prev , mpfi_prec ); mpfi_set(res_a_prev, op1);
-    mpfi_init2( res_g      , mpfi_prec );
-    mpfi_init2( res_g_prev , mpfi_prec ); mpfi_set(res_g_prev, op2);
-    mpfi_init2( tmp_a      , mpfi_prec );
-    mpfi_init2( tmp_g      , mpfi_prec );
-    mpfi_init2( union_ag   , mpfi_prec );
-
-    for (mpfr_prec_t i = 0; i < mpfi_prec; i++)
-    {
-        mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, res_a_prev, res_g_prev);
-
-        mpfi_intersect(intersection_ag, res_a, res_g);
-
-        if ( mpfi_is_empty(intersection_ag) )
-        {
-            mpfi_set(res_a_prev, res_a);
-            mpfi_set(res_g_prev, res_g);
-
-            continue;
-        }
-
-        break;
-    }
-
-    mpfi_union(res, res_a, res_g);
-
-    mpfi_clear( intersection_ag );
-    mpfi_clear( res_a           );
-    mpfi_clear( res_a_prev      );
-    mpfi_clear( res_g           );
-    mpfi_clear( res_g_prev      );
-    mpfi_clear( tmp_a           );
-    mpfi_clear( tmp_g           );
-    mpfi_clear( union_ag        );
-}
-
-
-
-static void mpfi_agm_step(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_srcptr op1, mpfi_srcptr op2)
-{
-    mpfi_t tmp_a, tmp_g;
-
-    mpfi_init2(tmp_a, mpfi_get_prec(res_a));
-    mpfi_init2(tmp_g, mpfi_get_prec(res_g));
-
-    mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, op1, op2);
-
-    mpfi_clear(tmp_a);
-    mpfi_clear(tmp_g);
-}
+void mpfi_agm_step_tmp(mpfi_ptr res_a, mpfi_ptr res_g, mpfi_ptr tmp_a, mpfi_ptr tmp_g, mpfi_srcptr op1, mpfi_srcptr op2);
 
 #endif /* MPFI_AGM_H */
