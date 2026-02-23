e45b85154c496d1ce85ce75f454d4f3af546ca80
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-23 14:17:16 +0900

    feat(demo): enhance demo_steps with full interval diagnostics
    - print x/y endpoints and widths
    - print union and intersection intervals
    - detect when union stops shrinking
    - adjust argument order in main for symmetry

--- examples/demo_steps.c
+++ examples/demo_steps.c
@@ -10,95 +10,123 @@
 
 void demo_steps(const mpfr_prec_t mpfi_prec, const unsigned long init_x, const unsigned long init_y)
 {
+    mpfr_t wid_x, wid_y, wid_intersection, wid_union;
 
     mpfi_t x, y;
     mpfi_t intersection_xy, union_xy, union_xy_prev;
     mpfi_t res_a, res_g;
     mpfi_t tmp_a, tmp_g;
-    mpfr_t width;
 
     // Initialize all variables
-    mpfi_init2( x               , mpfi_prec );
-    mpfi_init2( y               , mpfi_prec );
-    mpfi_init2( intersection_xy , mpfi_prec );
-    mpfi_init2( union_xy        , mpfi_prec );
-    mpfi_init2( union_xy_prev   , mpfi_prec );
-    mpfi_init2( res_a           , mpfi_prec );
-    mpfi_init2( res_g           , mpfi_prec );
-    mpfi_init2( tmp_a           , mpfi_prec );
-    mpfi_init2( tmp_g           , mpfi_prec );
-    mpfr_init2( width           , mpfi_prec );
+    mpfi_init2( x                , mpfi_prec );
+    mpfi_init2( y                , mpfi_prec );
+    mpfi_init2( intersection_xy  , mpfi_prec );
+    mpfi_init2( union_xy         , mpfi_prec );
+    mpfi_init2( union_xy_prev    , mpfi_prec );
+    mpfi_init2( res_a            , mpfi_prec );
+    mpfi_init2( res_g            , mpfi_prec );
+    mpfi_init2( tmp_a            , mpfi_prec );
+    mpfi_init2( tmp_g            , mpfi_prec );
+    mpfr_init2( wid_x            , mpfi_prec );
+    mpfr_init2( wid_y            , mpfi_prec );
+    mpfr_init2( wid_intersection , mpfi_prec );
+    mpfr_init2( wid_union        , mpfi_prec );
 
     // Initial values
     mpfi_set_ui(x, init_x);
     mpfi_set_ui(y, init_y);
-    mpfi_union(union_xy_prev, x, y);
 
-    // Print header
-    printf("===================================================================================================\n");
-    printf("iter interval 1               interval 2               union interval           width       overlap\n");
-    printf("---------------------------------------------------------------------------------------------------\n");
 
-    // Evaluate agm sequence
+
     for (mpfr_prec_t i = 0; i <= mpfi_prec; i++)
     {
-        mpfi_intersect(intersection_xy, x, y);
-        
-        // Print iteration number and intervals
-        mpfr_printf("%4ld ", i);
-        mpfr_printf("[%7.4Re, %7.4Re] ", &y->left, &y->right);
-        mpfr_printf("[%7.4Re, %7.4Re] ", &x->left, &x->right);
-        
+        mpfi_intersect (intersection_xy , x, y);
+        mpfi_union     (union_xy        , x, y);
+
         if ( mpfi_is_empty(intersection_xy) )
         {
-            printf("#disjoint!               #N/A        No\n");
+            mpfr_set_nan( &( intersection_xy->left  ) );
+            mpfr_set_nan( &( intersection_xy->right ) );
+        }
+
+
+
+        mpfi_diam_abs( wid_x            ,              x  );
+        mpfi_diam_abs( wid_y            ,               y );
+        mpfi_diam_abs( wid_intersection , intersection_xy );
+        mpfi_diam_abs( wid_union        , union_xy        );
+
+
+
+        printf("STEP.%ld\n", i);
+
+        mpfr_printf( "x.right  %Re\n", &( x->right         ) );
+        mpfr_printf( "x.left   %Re\n", &( x->left          ) );
+        mpfr_printf( "y.right  %Re\n", &( y->right         ) );
+        mpfr_printf( "y.left   %Re\n", &( y->left          ) );
+
+        printf("\n");
+
+        mpfr_printf( "u.right  %Re\n", &( union_xy        ->right ) );
+        mpfr_printf( "i.right  %Re\n", &( intersection_xy ->right ) );
+        mpfr_printf( "i.left   %Re\n", &( intersection_xy ->left  ) );
+        mpfr_printf( "u.left   %Re\n", &( union_xy        ->left  ) );
+
+        printf("\n");
+
+        mpfr_printf( "x.width  %Re\n" , &( wid_x            ) );
+        mpfr_printf( "y.width  %Re\n" , &( wid_y            ) );
+        mpfr_printf( "i.width  %Re\n" , &( wid_intersection ) );
+        mpfr_printf( "u.width  %Re\n" , &( wid_union        ) );
+
+        printf("u.inside ");
+
+        if ( mpfi_nan_p(union_xy_prev) )
+        {
+            printf("#N/A");
+        }
+        else if ( mpfi_is_strictly_inside(union_xy, union_xy_prev) )
+        {
+            printf("true");
         }
         else
         {
-            mpfi_union(union_xy, x, y);
-            mpfi_diam_abs(width, union_xy);
-            
-            mpfr_printf("[%7.4Re, %7.4Re] %7.4Re  YES", &union_xy->left, &union_xy->right, width);
-            
-            // Check convergence
-            if ( mpfi_is_strictly_inside(union_xy, union_xy_prev) ) {
-                printf(" (shrinking)");
-            }
-
-            printf("\n");
-
-            mpfi_set(union_xy_prev, union_xy);
+            printf("false\n\n"); break;
         }
 
-        // AGM step
+        printf("\n\n");
+
+
         mpfi_agm_step_tmp(res_a, res_g, tmp_a, tmp_g, x, y);
-        mpfi_set(x, res_a);
-        mpfi_set(y, res_g);
+
+        mpfi_set(      x       , res_a    );
+        mpfi_set(       y      , res_g    );
+        mpfi_set(union_xy_prev , union_xy );
     }
 
-    // Print footer
-    printf("===================================================================================================\n\n");
-
     // Cleanup
-    mpfi_clear( x               );
-    mpfi_clear( y               );
-    mpfi_clear( intersection_xy );
-    mpfi_clear( union_xy        );
-    mpfi_clear( union_xy_prev   );
-    mpfi_clear( res_a           );
-    mpfi_clear( res_g           );
-    mpfi_clear( tmp_a           );
-    mpfi_clear( tmp_g           );
-    mpfr_clear( width           );
+    mpfi_clear( x                );
+    mpfi_clear( y                );
+    mpfi_clear( intersection_xy  );
+    mpfi_clear( union_xy         );
+    mpfi_clear( union_xy_prev    );
+    mpfi_clear( res_a            );
+    mpfi_clear( res_g            );
+    mpfi_clear( tmp_a            );
+    mpfi_clear( tmp_g            );
+    mpfr_clear( wid_x            );
+    mpfr_clear( wid_y            );
+    mpfr_clear( wid_intersection );
+    mpfr_clear( wid_union        );
 }
 
 int main(void)
 {
     for (unsigned long i = 1; i < 6; i++)
     {
-        demo_steps(  53, 1UL, i );
-        demo_steps( 113, 1UL, i );
-        demo_steps( 237, 1UL, i );
+        demo_steps(  53, i, 1UL );
+        demo_steps( 113, i, 1UL );
+        demo_steps( 237, i, 1UL );
     }
 
     return EXIT_SUCCESS;
