92c6211d0515c4b3075ac6cc9a0fc9f2f8655d7f
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-02-25 06:59:57 +0900

    fix: initialize intersection_ag with mpfi_init2 instead of mpfi_init_set

--- src/mpfi_agm.c
+++ src/mpfi_agm.c
@@ -8,14 +8,16 @@ void mpfi_agm(mpfi_ptr res, mpfi_srcptr op1, mpfi_srcptr op2)
 
     mpfi_t intersection_ag, res_a, res_a_prev, res_g, res_g_prev, tmp_a, tmp_g;
 
-    mpfi_init_set(intersection_ag, res);
+    mpfi_init2( intersection_ag , mpfi_prec );
+    mpfi_init2( res_a           , mpfi_prec );
+    mpfi_init2( res_a_prev      , mpfi_prec );
+    mpfi_init2( res_g           , mpfi_prec );
+    mpfi_init2( res_g_prev      , mpfi_prec );
+    mpfi_init2( tmp_a           , mpfi_prec );
+    mpfi_init2( tmp_g           , mpfi_prec );
 
-    mpfi_init2( res_a      , mpfi_prec );
-    mpfi_init2( res_a_prev , mpfi_prec ); mpfi_set(res_a_prev, op1);
-    mpfi_init2( res_g      , mpfi_prec );
-    mpfi_init2( res_g_prev , mpfi_prec ); mpfi_set(res_g_prev, op2);
-    mpfi_init2( tmp_a      , mpfi_prec );
-    mpfi_init2( tmp_g      , mpfi_prec );
+    mpfi_set(res_a_prev, op1);
+    mpfi_set(res_g_prev, op2);
 
     for (mpfr_prec_t i = 0; i < mpfi_prec; i++)
     {
