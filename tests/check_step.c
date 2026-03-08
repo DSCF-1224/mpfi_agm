19f42c66ebaf0ceee47cd43ac06485308b605772
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-03-09 06:56:56 +0900

    Squashed commit of the following:
    commit c82f5697c2ed091803843887675a058d64d76971
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Mar 9 06:48:12 2026 +0900
    
        test: add decimal point interval cases to test_diff_input
    
        Add combinations of x = i/10 and y = j/10 (0 <= i < j <= 10) to complement the existing integer point interval cases.
    
    commit a2fa66fc1b82faea055c7698a17de0a4f8ba10ef
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Mar 9 06:36:27 2026 +0900
    
        refactor: split test_diff_input_unit into half and unit for symmetry
    
    commit a1b09e39796df9bce04c85a019fa63d082ed695d
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Mar 9 06:32:03 2026 +0900
    
        feat: add test_diff_input to validate agm_step with distinct inputs
    
        Verify that mpfi_agm_step correctly computes arithmetic and geometric means for distinct point interval inputs.
        Tests integer pairs from [0,5] and combinations with [+inf, +inf] (excluding i=0 to avoid agm(0, +inf) = NaN).
    
    commit 105896a24a2750323878a1774ae296d7c3f6b35a
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun Mar 8 13:03:24 2026 +0900
    
        feat: add test_diff_input_unit to validate arithmetic and geometric mean
    
    commit 3c4b6547cbc9fe8abeeaa02f3e6c0ef8f262cb79
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun Mar 8 12:50:58 2026 +0900
    
        feat: add printf_res_diff_input for diagnostic output in diff input tests
    
    commit 55e6ca5c25f97bb063be036993d22d878680f38c
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Mar 7 21:40:50 2026 +0900
    
        refactor: rename printf_res to printf_res_same_input in check_step
    
    commit 97fd6a81a93b63d471f6953a6803c3ebb26e9b43
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Mar 7 15:24:18 2026 +0900
    
        refactor: rename test_same_input_case to test_same_input_unit for consistency
    
    commit 86808109ec908169425fd71b19bbee90cb1d83b6
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Fri Mar 6 07:11:41 2026 +0900
    
        style: normalize spacing in check_step
    
    commit 0a17de42b66755b5c00ae864d1626dac8e18a7e4
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Fri Mar 6 07:05:14 2026 +0900
    
        refactor: improve assertion diagnostics in test_same_input_case_ui
    
    commit d812fda92852b1ba8651083e055ccd1aed53acbe
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Fri Mar 6 06:50:35 2026 +0900
    
        refactor: extract printf_res and improve assertion diagnostics in check_step
    
        Extract diagnostic output into printf_res for consistency with other test files.
        Consolidate assertions to print input and results on failure.
    
    commit a6e6a0434a39be5d3c0679d4b6761e495c18f897
    Merge: 95de445 720509d
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Thu Mar 5 07:11:28 2026 +0900
    
        Merge branch 'develop' into develop_test

--- tests/check_step.c
+++ tests/check_step.c
@@ -6,10 +6,78 @@
 #include <mpfi.h>
 #include "../src/mpfi_agm.h"
 #include "mpfi_agm_test_prec.h"
+#include "printf_interval.h"
 
 
 
-void test_same_input_case(const mpfi_srcptr input)
+int printf_res_diff_input(mpfr_srcptr mpfr_res, mpfi_srcptr mpfi_res, mpfi_srcptr op1, mpfi_srcptr op2, const char* name)
+{
+    char finalized_name[9];
+
+    snprintf(finalized_name, sizeof(finalized_name), "mpfi.%s", name);
+
+    printf("\n");
+
+    printf_interval( "op1     "     , op1      );
+    printf_interval( "op2     "     , op2      );
+    printf_interval( finalized_name , mpfi_res );
+
+    mpfr_printf("mpfr.%s :  %Rf\n", name, mpfr_res);
+
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
+
+
+
+int printf_res_same_input(mpfi_srcptr input, mpfi_srcptr res_a, mpfi_srcptr res_g)
+{
+    printf("\n");
+
+    printf_interval("input", input);
+    printf_interval("ari  ", res_a);
+    printf_interval("geo  ", res_g);
+
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
+
+
+
+void test_diff_input_unit_half(mpfr_ptr mpfr_res_a, mpfr_ptr mpfr_res_g, mpfi_ptr mpfi_res_a, mpfi_ptr mpfi_res_g, mpfi_srcptr op1, mpfi_srcptr op2)
+{
+    mpfi_agm_step(mpfi_res_a, mpfi_res_g, op1, op2);
+
+
+
+    // validation: arithmetic mean
+
+    mpfr_add(mpfr_res_a, &(op1->right), &(op2->right), MPFR_RNDU);
+    mpfr_div_ui(mpfr_res_a, mpfr_res_a, 2, MPFR_RNDU);
+    assert( mpfi_is_inside_fr(mpfr_res_a, mpfi_res_a) || printf_res_diff_input(mpfr_res_a, mpfi_res_a, op1, op2, "ari") );
+
+
+
+    // validation: geometric mean
+
+    mpfr_mul(mpfr_res_g, &(op1->left), &(op2->left), MPFR_RNDD);
+    mpfr_sqrt(mpfr_res_g, mpfr_res_g, MPFR_RNDD);
+    assert( mpfi_is_inside_fr(mpfr_res_g, mpfi_res_g) || printf_res_diff_input(mpfr_res_g, mpfi_res_g, op1, op2, "geo") );
+}
+
+
+
+void test_diff_input_unit(mpfr_ptr mpfr_res_a, mpfr_ptr mpfr_res_g, mpfi_ptr mpfi_res_a, mpfi_ptr mpfi_res_g, mpfi_srcptr op1, mpfi_srcptr op2)
+{
+    test_diff_input_unit_half(mpfr_res_a, mpfr_res_g, mpfi_res_a, mpfi_res_g, op1, op2);
+    test_diff_input_unit_half(mpfr_res_a, mpfr_res_g, mpfi_res_a, mpfi_res_g, op2, op1);
+}
+
+
+
+void test_same_input_unit(const mpfi_srcptr input)
 {
     mpfi_t res_a, res_g;
 
@@ -24,13 +92,11 @@ void test_same_input_case(const mpfi_srcptr input)
 
     if ( mpfi_nan_p(input) )
     {
-        assert( mpfi_nan_p(res_a) );
-        assert( mpfi_nan_p(res_g) );
+        assert( ( mpfi_nan_p(res_a) && mpfi_nan_p(res_g) ) || printf_res_same_input(input, res_a, res_g) );
     }
     else
     {
-        assert( mpfi_cmp(res_a, input) == 0 );
-        assert( mpfi_cmp(res_g, input) == 0 );
+        assert( ( ( mpfi_cmp(res_a, input) == 0 ) && ( mpfi_cmp(res_g, input) == 0 ) ) || printf_res_same_input(input, res_a, res_g) );
     }
 
     mpfi_clear(res_a);
@@ -39,7 +105,7 @@ void test_same_input_case(const mpfi_srcptr input)
 
 
 
-void test_same_input_case_ui(const mpfr_prec_t mpfi_prec, const unsigned long input)
+void test_same_input_unit_ui(const mpfr_prec_t mpfi_prec, const unsigned long input)
 {
     mpfi_t x, res_a, res_g;
 
@@ -54,12 +120,75 @@ void test_same_input_case_ui(const mpfr_prec_t mpfi_prec, const unsigned long in
 
     mpfi_agm_step(res_a, res_g, x, x);
 
-    assert( mpfi_cmp_ui(res_a, input) == 0 );
-    assert( mpfi_cmp_ui(res_g, input) == 0 );
+    assert( ( ( mpfi_cmp_ui(res_a, input) == 0 ) && ( mpfi_cmp_ui(res_g, input) == 0 ) ) || printf_res_same_input(x, res_a, res_g) );
 
-    mpfi_clear(x);
-    mpfi_clear(res_a);
-    mpfi_clear(res_g);
+    mpfi_clear( x     );
+    mpfi_clear( res_a );
+    mpfi_clear( res_g );
+}
+
+
+
+void test_diff_input(const mpfr_prec_t mpfi_prec)
+{
+    mpfr_t mpfr_res_a, mpfr_res_g;
+    mpfi_t mpfi_res_a, mpfi_res_g, x, y;
+
+
+
+    mpfr_init2( mpfr_res_a , mpfi_prec );
+    mpfr_init2( mpfr_res_g , mpfi_prec );
+
+    mpfi_init2( mpfi_res_a , mpfi_prec );
+    mpfi_init2( mpfi_res_g , mpfi_prec );
+    mpfi_init2( x          , mpfi_prec );
+    mpfi_init2( y          , mpfi_prec );
+
+
+
+    for (unsigned long i = 0; i <= 5; i++)
+    {
+        mpfi_set_ui(x, i);
+
+        for (unsigned long j = (i + 1); j <= 5; j++)
+        {
+            mpfi_set_ui(y, j);
+
+            test_diff_input_unit(mpfr_res_a, mpfr_res_g, mpfi_res_a, mpfi_res_g, x, y);
+        }
+
+        if (i > 0)
+        {
+            mpfr_set_inf( &( y->left  ), 1 );
+            mpfr_set_inf( &( y->right ), 1 );
+
+            test_diff_input_unit(mpfr_res_a, mpfr_res_g, mpfi_res_a, mpfi_res_g, x, y);
+        }
+    }
+
+
+
+    for (int i = 0; i <= 10; i++)
+    {
+        mpfi_set_d( x, ((double)i)/10 );
+
+        for (int j = (i + 1); j <= 10; j++)
+        {
+            mpfi_set_d( y, ((double)j)/10 );
+
+            test_diff_input_unit(mpfr_res_a, mpfr_res_g, mpfi_res_a, mpfi_res_g, x, y);
+        }
+    }
+
+
+
+    mpfr_clear( mpfr_res_a );
+    mpfr_clear( mpfr_res_g );
+
+    mpfi_clear( mpfi_res_a );
+    mpfi_clear( mpfi_res_g );
+    mpfi_clear( x          );
+    mpfi_clear( y          );
 }
 
 
@@ -74,7 +203,7 @@ void test_same_input(const mpfr_prec_t mpfi_prec)
 
     for (unsigned long i = 0; i < 6; i++)
     {
-        test_same_input_case_ui(mpfi_prec, i);
+        test_same_input_unit_ui(mpfi_prec, i);
     }
 
 
@@ -84,29 +213,29 @@ void test_same_input(const mpfr_prec_t mpfi_prec)
     {
         mpfi_interv_d(input, ((double)i)/10, ((double)j)/10);
 
-        test_same_input_case(input);
+        test_same_input_unit(input);
     }
 
 
 
-    mpfr_set_zero ( &input->left  , 1 );
-    mpfr_set_inf  ( &input->right , 1 );
+    mpfr_set_zero ( &( input->left  ) , 1 );
+    mpfr_set_inf  ( &( input->right ) , 1 );
 
-    test_same_input_case(input);
-
-
-
-    mpfr_set_inf( &input->left  , 1 );
-    mpfr_set_inf( &input->right , 1 );
-
-    test_same_input_case(input);
+    test_same_input_unit(input);
 
 
 
-    mpfr_set_nan(&input->left);
-    mpfr_set_nan(&input->right);
+    mpfr_set_inf( &( input->left  ) , 1 );
+    mpfr_set_inf( &( input->right ) , 1 );
 
-    test_same_input_case(input);
+    test_same_input_unit(input);
+
+
+
+    mpfr_set_nan( &( input->left  ) );
+    mpfr_set_nan( &( input->right ) );
+
+    test_same_input_unit(input);
 
 
 
@@ -118,6 +247,7 @@ void test_same_input(const mpfr_prec_t mpfi_prec)
 void test_per_prec(const mpfr_prec_t mpfi_prec)
 {
     test_same_input(mpfi_prec);
+    test_diff_input(mpfi_prec);
 }
 
 
