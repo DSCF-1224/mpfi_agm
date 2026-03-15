aa46ceeb370d43e9c3b9de3fb9b44f00f7fbaabe
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-03-15 15:21:13 +0900

    test: add [-inf, -0] and [-inf, +0] combinations to check_crossing_zero

--- tests/check_crossing_zero.c
+++ tests/check_crossing_zero.c
@@ -75,6 +75,26 @@ void test_per_prec(const mpfr_prec_t prec)
 
 
 
+        // [ neg, pos] x [-inf,  -0]
+        // [-inf,  -0] x [ neg, pos]
+
+        mpfr_set_inf  ( &( y->left  ), -1 );
+        mpfr_set_zero ( &( y->right ), -1 );
+
+        test_crossing_zero_unit(res, x, y);
+
+
+
+        // [ neg, pos] x [-inf,  +0]
+        // [-inf,  +0] x [ neg, pos]
+
+        mpfr_set_inf  ( &( y->left  ), -1 );
+        mpfr_set_zero ( &( y->right ),  1 );
+
+        test_crossing_zero_unit(res, x, y);
+
+
+
         // [neg,  pos] x [ -0, +inf]
         // [ -0, +inf] x [neg,  pos]
 
